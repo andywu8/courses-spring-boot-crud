c9e03c2744ab1727824c5898bc4cd17e4cc30e07
Andy Wu <andyww09@gmail.com>
2023-10-30 14:16:38 -0700

    init

---
+++ src/main/java/com/springboot/springbootfullstackcrud/SpringBootFullstackCrudApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.springbootfullstackcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFullstackCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFullstackCrudApplication.class, args);
+	}
+
+}
